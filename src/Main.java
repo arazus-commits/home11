718819a9a9e210db66924cb51be892686477cb3c
arazus-commits <araz.osmanov@gmail.com>
2026-04-08 23:42:12 +0300

    пуш

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,55 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+class Homework {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int year2 = 2024;
+        task1(year2);
+        int clientOS2 = 1;
+        int clientDeviceYear = 2014;
+        task2(clientOS2, clientDeviceYear);
+        int deliveryDistance = 70;
+        int totalDay = task3(deliveryDistance);
+        System.out.println(totalDay);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void task1(int year2) {
+        if (year2 < 1584) {
+            System.out.println(", високосный год еще не был принят.");
+        } else if ((year2 % 4 == 0 && year2 % 100 != 0) || year2 % 400 == 0) {
+            System.out.println(year2 + " год - високосный год");
+        } else {
+            System.out.println(year2 + " год - невисокосный год");
         }
     }
+
+    public static void task2(int clientOS2, int clientDeviceYear) {
+        if (clientOS2 == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (clientOS2 == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS2 == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для IOS по ссылке");
+        } else if (clientOS2 == 0) {
+            System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+        }
+    }
+
+    public static int task3(int deliveryDistance) {
+        int day = 0;
+        if (deliveryDistance <= 20) {
+            day += 1;
+            System.out.println("Потребуется дней: " + day);
+        } else if (deliveryDistance <= 60) {
+            day += 2;
+            System.out.println("Потребуется дней: " + day);
+        } else if (deliveryDistance <= 100) {
+            day += 3;
+            System.out.println("Потребуется дней: " + day);
+        } else {
+            System.out.println("Доставка не осуществляется");
+
+        }
+        return day;
+
+    }
 }
+
